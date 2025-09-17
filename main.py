ada5b088f4f708edc41ce3f26f1ae76a67698c50
Fidos7 <kozacek.adrian@gmail.com>
2025-09-17 10:16:17 +0200

    Title commit

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 import tkinter as tk
 import string
 import random
+
+from main1 import nadpis
+
+okno = tk.Tk()
+okno.title("Generátor Hesla")
+okno.geometry("360x260")
+
+nadpis = tk.Label(okno, text="Vygeneruj Heslo", font=("Arial", 20))
+nadpis.pack(pady=10)
