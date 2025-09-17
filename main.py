668b2a63e7e8bb457d1bd931c37722143668dc86
Fidos7 <kozacek.adrian@gmail.com>
2025-09-17 11:57:41 +0200

    button commit

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import tkinter as tk
 import string
 import random
 
-from main1 import nadpis
+from main1 import nadpis, button_password
 
 okno = tk.Tk()
 okno.title("Generátor Hesla")
@@ -10,3 +10,19 @@ okno.geometry("360x260")
 
 nadpis = tk.Label(okno, text="Vygeneruj Heslo", font=("Arial", 20))
 nadpis.pack(pady=10)
+
+def passwordGenerator():
+    znaky = string.ascii_letters + string.digits + string.punctuation
+    password = "".join(random.choice(znaky) for x in range(12))
+    passwordDone.config(text=password)
+
+frame_password = tk.Frame(okno)
+frame_password.pack(pady=5)
+
+button_password = tk.Button(frame_password, text="Generuj", command=lambda: passwordGenerator())
+button_password.pack(side="left", padx=5)
+
+passwordDone = tk.Label(okno, text="")
+passwordDone.pack(side="left")
+
+okno.mainloop()
